1d7eb4c7170a11247d810f7ff9723d88c6bbfa1f
fabiandiaz416 <diazfabian99@icloud.com>
2025-07-29 20:34:30 -0300

    feat: integrate orders module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [RestaurantsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
