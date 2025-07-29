570a51a72c053b77e4670d334146e5f879b512ac
fabiandiaz416 <diazfabian99@icloud.com>
2025-07-29 20:35:14 -0300

    feat: add restaurants service

---
+++ backend/src/restaurants/restaurants.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { Restaurant } from './entities/restaurant.entity';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class RestaurantsService {
+  private readonly restaurants: Restaurant[] = [];
+
+  constructor() {
+    for (let i = 0; i < 100; i++) {
+      const products: Product[] = [];
+      for (let j = 0; j < Math.floor(Math.random() * 100) + 20; j++) {
+        products.push({
+          id: faker.string.uuid(),
+          name: faker.commerce.productName(),
+          price: parseFloat(faker.commerce.price()),
+        });
+      }
+      this.restaurants.push({
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        products,
+      });
+    }
+  }
+
+  findAll(page: number, limit: number) {
+    const start = (page - 1) * limit;
+    const end = page * limit;
+  
+    const paginatedRestaurants = this.restaurants
+      .slice(start, end)
+      .map(({ products, ...restaurant }) => restaurant); 
+  
+    return {
+      data: paginatedRestaurants,
+      hasMore: end < this.restaurants.length,
+    };
+  }
+
+  findProducts(id: string, page: number, limit: number) {
+    const restaurant = this.restaurants.find((r) => r.id === id);
+    if (!restaurant) {
+      return { data: [], hasMore: false };
+    }
+    const start = (page - 1) * limit;
+    const end = page * limit;
+    const paginatedProducts = restaurant.products.slice(start, end);
+    return {
+      data: paginatedProducts,
+      hasMore: end < restaurant.products.length,
+    };
+  }
+}
