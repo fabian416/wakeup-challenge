d63e56b3d6ef06076a044d7daeb3f4a04cc18b97
fabiandiaz416 <diazfabian99@icloud.com>
2025-07-30 20:06:57 -0300

    add prefix api into orders.controller

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
-@Controller('orders')
+@Controller('api/orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
